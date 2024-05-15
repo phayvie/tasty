adb8366083ef39984ff1b23a64753d3b796b6f6f
phayvie <onyeaghalafavourr.com>
2024-05-15 10:42:54 +0100

    nav bar

---
+++ script.js
@@ -0,0 +1,13 @@
+var cancle = document.querySelector('.cancle');
+var navBar = document.querySelector('.nav-bar');
+var menu = document.querySelector('.menu');
+
+cancle.addEventListener("click", function() {
+    navBar.classList.toggle("show")
+ 
+});
+
+menu.addEventListener("click", function() {
+    navBar.classList.toggle("show")
+ 
+    });
